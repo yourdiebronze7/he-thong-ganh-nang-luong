4a14035bf8e11bbc4814b8d477cef76a1908ef34
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-26 11:42:07 +0000

    Add require statement to validate transaction amount and price in makeTransaction function.

--- contract.sol
+++ contract.sol
@@ -14,6 +14,8 @@ contract EnergyExchange {
     event TransactionMade(address indexed buyer, address indexed seller, uint256 amount, uint256 price);
 
     function makeTransaction(address _seller, uint256 _amount, uint256 _price) public {
+        require(_amount > 0, "Amount must be greater than zero.");
+        require(_price > 0, "Price must be greater than zero.");
         transactionCount++;
         transactions[transactionCount] = Transaction(msg.sender, _seller, _amount, _price);
         emit TransactionMade(msg.sender, _seller, _amount, _price);
