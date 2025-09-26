6773825e852ff020d92bf967c9ca501582c674bc
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-26 11:42:38 +0000

    Improve response message for root route

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.send('Energy Load Balancing API');
+    res.send('Welcome to the Energy Load Balancing API');
 });
 
 app.listen(PORT, () => {
