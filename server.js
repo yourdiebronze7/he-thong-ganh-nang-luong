3385efad796c6beb7546859b11369308d47279aa
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-26 11:41:24 +0000

    feat: initial commit for server.js

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send('Hệ Thống Gánh Năng Lượng API');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
