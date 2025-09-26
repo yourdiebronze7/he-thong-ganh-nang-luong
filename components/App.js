5428d638ec32efb80fa254ad781f5ff04bdae509
yourdiebronze7 <sidnyjernov@hotmail.com>
2025-09-26 11:41:28 +0000

    feat: initial commit for components/App.js

---
+++ components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hệ Thống Gánh Năng Lượng</h1>
+      <p>Chào mừng bạn đến với nền tảng chia sẻ năng lượng tái tạo!</p>
+    </div>
+  );
+};
+
+export default App;
